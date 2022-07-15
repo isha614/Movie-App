52df350ade86857e1be2f2f1eddcd241b6ddd0ae
Isha Mehta <71143297+isha614@users.noreply.github.com>
2022-07-15 19:43:39 +0530

    styling of bottom nav

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -2,26 +2,34 @@ import React from 'react';
 import { Box } from '@material-ui/core';
 import  BottomNavigation  from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import MovieIcon from '@material-ui/icons/Movie';
+import TvIcon from '@material-ui/icons/Tv';
+import SearchIcon from '@material-ui/icons/Search';
 
 export default function SimpleBottomNavigation() {
   const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+ 
+   
 
   return (
-    <Box sx={{ width: 500 }}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+       <BottomNavigation value={value} onChange={handleChange}>
+        <BottomNavigationAction 
+        style={{color:"black"}}
+        label="Trending" icon={<WhatshotIcon />} />
+        <BottomNavigationAction
+         style={{color:"black"}}
+         label="Movies" icon={<MovieIcon/>} />
+        <BottomNavigationAction 
+         style={{color:"black"}}
+        label="TV Series" icon={<TvIcon />} />
+         <BottomNavigationAction 
+         style={{color:"black"}}
+        label="Search" icon={<SearchIcon />} />
       </BottomNavigation>
-    </Box>
+    
   );
 }
