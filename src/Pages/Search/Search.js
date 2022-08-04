dcbf1fd7e5bdc5fa60883ee7a7cc2364f0ba18c9
Isha Mehta <71143297+isha614@users.noreply.github.com>
2022-08-04 21:47:53 +0530

    series & search

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,10 +1,30 @@
-import React from "react";
-
+import React,{useState} from "react";
+import { TextField, Box } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/system";
+import SearchIcon from '@mui/icons-material/Search';
+import Button from '@mui/material/Button';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 const Search=()=>{
-    return(
-        <div>
-             <span className="pageTitle">Search</span>
-        </div>
-    )
+
+const[type,setType]=useState(0);
+const darkTheme=createTheme({
+    palette: {
+      mode:"dark",
+    },
+   })
+return(
+    <>
+    <div style={{display:'flex',margin:'15px 0'}}>
+<TextField 
+style={{flex:1}}
+id="standard-basic" label="Search" variant="standard" color="primary" />
+<Button variant='contained' style={{marginLeft: 10}}><SearchIcon/></Button>
+</div>
+<Tabs value={type} indicatorColor='primary' textColor="primary">
+<Tab style={{width:"50%"}} label="Search Movies"/>
+</Tabs>
+</>
+)
 }
 export default Search;
--- src/Pages/Series/Series.js
+++ src/Pages/Series/Series.js
@@ -1,10 +1,63 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import axios from "axios";
+import useGenres from "../../hooks/useGenre";
+import Genres from "../../components/Genres";
+import SingleContent from "../../components/SingleContent/SingleContent";
+import CustomPagination from "../../components/Pagination/CustomPagination";
+import './Series.css';
 
 const Series=()=>{
+const [page,setPage]=useState(1);
+const [content,setContent]=useState([]);
+const [numOfPages, setNumOfPages]=useState();
+
+const [selectedGenres,setSelectedGenres]=useState([]);
+const [genres,setGenres]=useState([]);
+
+const genreforURL=useGenres(selectedGenres);
+const fetchMovies= async()=>{
+    const {data} = await axios.get(
+        `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`)
+     console.log(data);
+     setContent(data.results);
+     setNumOfPages(data.total_pages);
+};
+
+useEffect(()=>{
+  fetchMovies();
+  // eslint-disable-next-line
+},[page, genreforURL]);
+
     return(
         <div>
  <span className="pageTitle">Series</span>
-        </div>
+ <Genres
+            type="tv"
+            selectedGenres={selectedGenres}
+            setSelectedGenres={setSelectedGenres}
+            genres={genres}
+            setGenres={setGenres}
+            setPage={setPage}
+            />
+ <div className="series">
+                {
+                    content && content.map((c)=>(
+                      <SingleContent 
+                      key={c.id}
+                      id={c.id}
+                      poster={c.poster_path}
+                     title={c.title || c.name}
+                     date={c.first_air_date || c.release_date}
+                     media_type="tv"
+                     vote_average={c.vote_average}
+                      />
+                    )) }
+            </div>
+            {numOfPages>1 && (
+            <CustomPagination setPage={setPage} numOfPages={numOfPages}/>
+            )}
+            </div>
+
     )
 }
 export default Series;
