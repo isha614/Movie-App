5212119f57f7a64b88da245226e1a5507609348d
Isha Mehta <71143297+isha614@users.noreply.github.com>
2022-08-05 22:44:51 +0530

    done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './components/Header/Header';
 import SimpleBottomNavigation from './components/MainNav';
 import Trending from './Pages/Trending/Trending';
 import Movies from './Pages/Movies/Movies';
-import Search from './Pages/Search/Search';
+
 import Series from './Pages/Series/Series';
 function App() {
   return (
@@ -19,7 +19,6 @@ function App() {
       <Route path='/' element={<Trending/>} exact/>
       <Route path='/movies' element={<Movies/>}/>
       <Route path='/series' element={<Series/>}/>
-      <Route path='/search' element={<Search/>}/>
     </Routes>
    </Container>
    </div>
